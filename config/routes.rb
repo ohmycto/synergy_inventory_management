8e7560eb963ca0916539c9f1d38beff3425643a9
Babur Usenakunov <bob.usenakunov@gmail.com>
2013-02-15 19:51:54 +0600

    Upgrading to 1.3.x initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-Rails.application.routes.draw do
+Spree::Core::Engine.routes.draw do
   namespace :admin do
     match 'edit_multiple_products' => "products#edit_multiple", :as => :edit_multiple_products_start, :via => :get
     match 'edit_multiple_products/:id' => "products#edit_multiple", :as => :edit_multiple_products, :via => :get
